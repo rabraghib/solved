7016b148d5f11b192252d333715c930079854d86
Rabyâ Raghib <rabraghib@gmail.com>
2020-07-19 23:14:01 +0100

    Create Corona Virus Alert.cpp

---
+++ arena/Corona Virus Alert.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string population;
+
+    cin >> population;
+
+    if (population.find('c') != string::npos) {
+        cout << "Stay Home";
+    }
+    else
+    {
+        cout << "Safe";
+    }
+
+    return 0;
+}
