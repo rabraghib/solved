8e93cd64a647ad2916a61aed82987416f727d643
Rabyâ Raghib <rabraghib@gmail.com>
2020-07-20 18:39:20 +0100

    Create Arena Servers.cpp

---
+++ arena/Arena Servers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    // https://arena.ioi.ma/problem/mdc1arenaservers
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++)
+    {
+        int N, C,numservers = 0;
+        cin >> N >> C;
+        for (int i = 0; i < N; i++)
+        {
+            long int a = 0.0;
+            cin >> a;
+            numservers += ceil(1.0 * a / C);
+        }
+        cout << numservers << endl;
+    }
+    return 0;
+}
