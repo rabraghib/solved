60f490e6791840fd6ad60811a8c226045dceb8d5
Rabyâ Raghib <rabraghib@gmail.com>
2020-07-20 11:59:56 +0100

    Create Help parent.cpp

---
+++ arena/Help parent.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int T, M;
+    string word;
+    cin >> T;
+    for (int i = 1; i <= T; i++)
+    {
+        cin >> M >> word;
+        string v_word = word.substr(0, M -1) + word.substr(M);
+        cout << i << " " << v_word << endl;
+    }
+}
