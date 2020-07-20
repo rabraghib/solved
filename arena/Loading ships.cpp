9820f7a8ceec2b0a90ff7dc00a7a4380729ae951
Rabyâ Raghib <rabraghib@gmail.com>
2020-07-20 18:38:45 +0100

    Create Loading ships.cpp

---
+++ arena/Loading ships.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int low = 1, high = 2000000000, mid = 0;
+    string state ;
+    while (state != "OK")
+    {
+        mid = low + ((high - low) / 2);
+        cout << mid << endl;
+        cin >> state;
+        if (state == "FLOATS")
+        {
+            low = mid + 1;
+        }
+        else if (state == "SINKS")
+        {
+            high = mid - 1;
+        }
+    }
+}
