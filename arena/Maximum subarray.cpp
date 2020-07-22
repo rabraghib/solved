b8490db59a21b317e6575e5a1d555952f4f2ea9a
Rabyâ Raghib <rabraghib@gmail.com>
2020-07-22 02:26:23 +0100

    Create Maximum subarray.cpp

---
+++ arena/Maximum subarray.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N, big_sum = 0, sum = 0, num;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> num;
+		sum += num;
+		if (sum < 0)
+			sum = 0;
+
+		if (sum > big_sum)
+			big_sum = sum;
+	}
+	cout << big_sum << endl;
+}
