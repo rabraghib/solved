0ddfdfab7c2c516dde743c2d1d87f3d1b4b51dc0
Rabyâ Raghib <rabraghib@gmail.com>
2020-07-31 21:36:21 +0100

    Create Fibonacci queries.cpp

---
+++ arena/Fibonacci queries.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int fibonacci(int N){
+	int res;
+	if (N <= 1)
+		return N;
+	res = fibonacci(N - 1) + fibonacci(N - 2);
+	return res;
+}
+
+int main()
+{
+	int Q, N;
+	cin >> Q;
+	for (int i = 0; i < Q; i++)
+	{
+		int N;
+		cin >> N;
+		cout << fibonacci(N) << endl;
+	}
+}
