172758d074ba81a55de2e0b8d7a6958d35f245fc
Rabyâ Raghib <rabraghib@gmail.com>
2021-04-21 18:44:07 +0000

    Solved: Acrobats

---
+++ arena/MOI 2021 Practice contest/Acrobats.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        long long L;
+        cin >> L;
+        int num_of_persons = (long long)(
+            (
+                ((long double)(L % 1000000007) / 2) 
+                * ((L-1) % 1000000007)
+            ) + 1
+        ) % 1000000007;
+        cout << num_of_persons << endl;
+    }
+}
